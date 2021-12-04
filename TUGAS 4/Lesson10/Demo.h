4211d089b323148f697f525cff6182309709c55f
idantb03 <81242358+idantb03@users.noreply.github.com>
2021-12-04 22:59:43 +0700

    Add files via upload

---
+++ TUGAS 4/Lesson10/Demo.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "RenderEngine.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <SOIL/SOIL.h>
+
+class Demo :
+	public RenderEngine
+{
+public:
+	Demo();
+	~Demo();
+private:
+	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2, VBO3, VAO3, EBO3, texture3, VBO4, VAO4, EBO4, texture4;
+	virtual void Init();
+	virtual void DeInit();
+	virtual void Update(double deltaTime);
+	virtual void Render();
+	virtual void ProcessInput(GLFWwindow *window);
+	void BuildStove();
+	void BuildSideStove();
+	void BuildFrontStove();
+	void BuildPlane();
+	void DrawStove();
+	void DrawSideStove();
+	void DrawFrontStove();
+	void DrawPlane();
+};
+
